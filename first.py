89723634286de0b1a7d6ffd78d8bd7a8e79f24fe
Mike <mixser.by@gmail.com>
2016-01-18 09:40:55 +0300

    Reuse cv2 split and merge instead of iterating by image and reverse colors

--- first.py
+++ first.py
@@ -3,10 +3,5 @@ import numpy
 
 image = cv2.imread('images/first.jpg')
 
-array = numpy.array(image)
-
-for line in array:
-    for pixel in line:
-        pixel[0], pixel[1], pixel[2] = pixel[2], pixel[1], pixel[0]
-
-cv2.imwrite('out/first.png', array)
+b, g, r = cv2.split(image)
+cv2.imwrite('out/first.png', cv2.merge((r, g, b)))
